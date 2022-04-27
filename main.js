cd1169ca8870077fc8de4ae5b5f838a6a9f610d5
ostermandiego <ostermandiego@outlook.com>
2022-04-27 20:09:56 -0300

    agregando contenidos

--- main.js
+++ main.js
@@ -5,23 +5,39 @@ const saludo = () => {
         nombre = prompt("Bienvenido a Tienda de Café, ingrese su nombre:");
     } while (nombre === "" || !isNaN(nombre));
 
-    console.log(`Hola ${nombre}`);
 };
 
+class bebida {
+    constructor(precio, tamaño, nombreBebida){
+        this.precio = precio
+        this.tamaño = tamaño
+        this.nombreBebida = nombreBebida
+    }
+}
+
+const cafe = new bebida(250, "grande", "cafe")
+const te = new bebida(250, "grande", "te")
+const capuccino = new bebida(300, "grande", "capuccino")
+const chocolate = new bebida(350, "grande", "chocolate")
+const latte = new bebida(400, "grande", "latte")
+const café_con_leche = new bebida(300, "grande", "cafe con leche")
+
+let arrayBebidas = [cafe, te, capuccino, chocolate, latte, café_con_leche]
+
 const Bebidas = () => {
-    let listbebidas = parseInt(prompt("¿Que bebida desea?\n1) Café\n2) Té\n3) Capuccino\n4) Chocolate\n5 Latte\n6) Café con leche "));
+    let listbebidas = parseInt(prompt(`¿Que bebida desea?\n1) ${arrayBebidas[0].nombreBebida}\n2) ${arrayBebidas[1].nombreBebida}\n3) ${arrayBebidas[2].nombreBebida}\n4) ${arrayBebidas[3].nombreBebida}\n5 ${arrayBebidas[4].nombreBebida}\n6) ${arrayBebidas[5].nombreBebida} `));
     if (listbebidas == 1) {
-        alert("Disfrute su café")
+        alert(`Disfrute su ${arrayBebidas[0].nombreBebida}`)
     } else if (listbebidas == 2) {
-        alert("Disfrute su té")
+        alert(`Disfrute su ${arrayBebidas[1].nombreBebida}`)
     } else if (listbebidas == 3) {
-        alert("Disfrute su capuccino")
+        alert(`Disfrute su ${arrayBebidas[2].nombreBebida}`)
     } else if (listbebidas == 4) {
-        alert("Disfrute su chocolate")
+        alert(`Disfrute su ${arrayBebidas[3].nombreBebida}`)
     } else if (listbebidas == 5) {
-        alert("Disfrute su latte")
+        alert(`Disfrute su ${arrayBebidas[4].nombreBebida}`)
     } else if (listbebidas == 6) {
-        alert("Disfrute su café con leche")
+        alert(`Disfrute su ${arrayBebidas[5].nombreBebida}`)
     } else {
         alert("Ingrese una opción válida")
     }

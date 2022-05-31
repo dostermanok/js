cdce74c1ea361b35cc92b874ee16322ccd198544
ostermandiego <ostermandiego@outlook.com>
2022-05-30 23:55:17 -0300

    mejorando el dom

--- main.js
+++ main.js
@@ -73,8 +73,8 @@ fetch(url)
 
     const mostrarData = (data) => {
         console.log (data)
-        let body = ''
-        for (let i = 0; i<data.lenght; i++){
+        let body = ""
+        for (let i = 0; i<data.length; i++){
             body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`
         }
         document.getElementById('data').innerHTML = body    
